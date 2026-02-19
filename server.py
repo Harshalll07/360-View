3fbaac09bee421e627ffe8adb038cbf8529d347f
Harshal Vinod Jadhav <138389903+Harshalll07@users.noreply.github.com>
2026-02-19 12:45:51 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,24 @@
+
+import http.server
+import socketserver
+import webbrowser
+import os
+
+PORT = 8080
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+print(f"Serving at http://localhost:{PORT}")
+print(f"Directory: {DIRECTORY}")
+
+# Open browser automatically
+webbrowser.open(f"http://localhost:{PORT}")
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nStopping server...")
